3fb0205dc79e566fe7f6a19a16bfe442baa5fb25
preint\jberke <joshua.berke@gmail.com>
2014-07-19 12:51:41 -0400

    Updated various things

---
+++ Sample/FormsGallery/Client/FormsGallery/FormsGallery/PerformancePage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsGallery
+{
+    class PerformancePage : BasePage
+    {
+        public PerformancePage()
+        {
+            Label header = new Label
+            {
+				Text = "Timing inside and out",
+                Font = Font.BoldSystemFontOfSize(NamedSize.Medium),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Spacing = 0,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = 
+                {
+                 
+                    new StackLayout
+                    {
+                        Spacing = 0,
+
+                        Orientation = StackOrientation.Horizontal,
+                        Children = 
+                        {
+                            new Label
+                            {
+                                Text = "User Behavior",
+								HorizontalOptions=LayoutOptions.CenterAndExpand
+                            },
+							new Button
+                            {
+                                Text = "Start",
+                                HorizontalOptions = LayoutOptions.Start
+                            },
+							new Button
+                            {
+                                Text = "Stop",
+								HorizontalOptions=LayoutOptions.End
+                            },
+                        }
+                    }
+                }
+            };
+
+            // Accomodate iPhone status bar.
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children = 
+                {
+                    header,
+                    stackLayout
+                }
+            };
+        }
+    }
+}
