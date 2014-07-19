9b8f942421b88bb0de8462c07e147f10b0f514d8
preint\jberke <joshua.berke@gmail.com>
2014-07-19 17:28:58 -0400

    Instance ID Generated from List, Basic UserName generated not tied in and needs more work

--- Sample/FormsGallery/Client/FormsGallery/FormsGallery/App.cs
+++ Sample/FormsGallery/Client/FormsGallery/FormsGallery/App.cs
@@ -9,6 +9,13 @@ namespace FormsGallery
 {
     public class App
     {
+        private static Random _rand = new Random();
+
+        private static string[] _keys = { "ABC-123", "XYZ-432", "EFG-234", "HIJ-012", "YUI-765", "ITI-912", "LKL-876", "TRE-432", "LIK-101", "TRE-111" };
+        private static string[] _userNames = { "Josh", "Fred", "Nathan", "Bill", "Mark", "Gabe", "Joe", "Pat", "Cindy", "Laura", "Emily" };
+
+        private static string _userName = _userNames[_rand.Next(0, _userNames.Length)];
+        private static string _licenseKey = GetLicenseKey();
         public static Page GetMainPage()
         {
 			
@@ -22,7 +29,33 @@ namespace FormsGallery
 
         public static void Start()
         {
-            PAClientFactory.StartApplication("MyInstance");
+            
+            PAClientFactory.StartApplication(_licenseKey);
+
+        }
+
+
+
+        public static string GetLicenseKey()
+        {
+            return _keys[_rand.Next(0, _keys.Length - 1)];
+
+        }
+
+        public static string LicenseKey
+        {
+            get
+            {
+                return _licenseKey;
+            }
+        }
+
+        public static string UserName
+        {
+            get
+            {
+                return _userName;
+            }
         }
     }
 }
